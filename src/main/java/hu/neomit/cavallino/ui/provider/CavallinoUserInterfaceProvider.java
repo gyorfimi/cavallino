b456dfc8b7bd28952f1aec59bb225a6f34fb95d9
miki <none@none>
2015-06-29 17:21:20 +0200

    starting point of ui.

---
+++ src/main/java/hu/neomit/cavallino/ui/provider/CavallinoUserInterfaceProvider.java
@@ -0,0 +1,18 @@
+package hu.neomit.cavallino.ui.provider;
+
+import hu.commit.prodigium.Named;
+import hu.commit.prodigium.base.Array;
+import hu.commit.prodigium.base.JSCollections;
+import hu.commit.prodigium.base.reactivity.ConnectTo;
+import hu.commit.prodigium.base.ui.ResourceLinkHandler;
+import hu.commit.prodigium.base.ui.UIElements;
+import hu.commit.prodigium.base.ui.UserInterfaceTemplate;
+import hu.commit.prodigium.base.ui.UserInterfaceTemplateProvider;
+import hu.commit.prodigium.base.ui.link.LinkHandler;
+import hu.commit.prodigium.modules.prodigium_sc.base.ui.ProdigiumSmartClientScreen;
+import org.stjs.javascript.Map;
+
+@Named("CavallinoUserInterfaceProvider")
+@ConnectTo("cavallino")
+public class CavallinoUserInterfaceProvider extends ProdigiumSmartClientScreen {
+}
